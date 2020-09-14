0a3ec057a260dc2727bea62bbbe50229e33dd719
djmajino <maros.valach@gmail.com>
2020-09-14 22:43:48 +0200

    Add files via upload

---
+++ initiate.js
@@ -0,0 +1,11 @@
+__meteor_runtime_config__ = {
+   "meteorRelease":"0.7.0.1",
+   "PUBLIC_SETTINGS":{
+      "ga":{
+         "account":"UA-42798810-2"
+      }
+   },
+   "ROOT_URL":"http://threesjs.com",
+   "ROOT_URL_PATH_PREFIX":"",
+   "autoupdateVersion":"3b8e7eadc8a69b46ebe486164ae87b351205ee07"
+};
